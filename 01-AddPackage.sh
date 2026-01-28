27135d9db7d6deaf1c588951d4ec0550502fee23
root <caseyriley94@gmail.com>
2026-01-28 05:08:10 +0000

    first-config_push

---
+++ 01-AddPackage.sh
@@ -0,0 +1,4 @@
+AddPackage tealdeer # Rust version of the tldr cheatsheet; Helpful snippets for cmd's
+AddPackage bat # Better cat, Adds borders and syntax highlighting/Prettified output
+AddPackage eza # Better ls; More cozy flags, and colored ouptut.
+AddPackage pkgfile # Pkgconfig tool; Set C-flags, libs top-level/install dirs and more for every package
