44befebccb926d5ee9c1e695d3773cbd3a0dacde
root <caseyriley94@gmail.com>
2026-01-29 22:21:37 +0000

    removals

--- 01-AddPackage.sh
+++
@@ -1,4 +0,0 @@
-AddPackage tealdeer # Rust version of the tldr cheatsheet; Helpful snippets for cmd's
-AddPackage bat # Better cat, Adds borders and syntax highlighting/Prettified output
-AddPackage eza # Better ls; More cozy flags, and colored ouptut.
-AddPackage pkgfile # Pkgconfig tool; Set C-flags, libs top-level/install dirs and more for every package
--- 02-RemovePackages.sh
+++
@@ -1,6 +0,0 @@
-RemovePackage avahi # Service Discovery for Linux using mDNS/DNS-SD (compatible with Bonjour)
-RemovePackage linux-atm # Drivers and tools to support ATM networking under Linux.
-RemovePackage mc # A file manager that emulates Norton Commander
-RemovePackage nano # Pico editor clone with enhancements
-RemovePackage nano-syntax-highlighting # Nano editor syntax highlighting enhancements
-RemovePackage wvdial # A dialer program to connect to the Internet;Because WTF!!
